a7c9e0ade2e8b1b6a8c07ae82d0d7abc278285bc
raidendotai <raidendotai@gmail.com>
2024-10-14 20:44:35 +0100

    :)*

--- modules/pglite.bak/contrib/pg_trgm.js
+++
@@ -1,2 +0,0 @@
-import{i as e}from"../chunk-Y3AVQXKT.js";e();var t=async(n,s)=>({bundlePath:new URL("../pg_trgm.tar.gz",import.meta.url)}),p={name:"pg_trgm",setup:t};export{p as pg_trgm};
-//# sourceMappingURL=pg_trgm.js.map
